d555165446e41eb794e2c9d04cf520f7fc6a95d2
Aikellanne <aikellanne18@gmail.com>
2025-08-08 16:52:36 -0300

    melhorando o js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const bntSalvar = document.getElementById("salvar");
 
 const inputTitulo = document.getElementById("titulo");
 const inputDescricao = document.getElementById("descricao");
+const calendario = document.getElementById("data");
 
 botaoTarefa.addEventListener ("click", function (){
     semTarefas.classList.add("oculto");
@@ -17,6 +18,33 @@ botaoTarefa.addEventListener ("click", function (){
 
 bntCancelar.addEventListener ("click", function (){
     formTarefa.classList.add("oculto");
-})
+});
 
+bntSalvar.addEventListener ("click", function (){
+    const titulo = inputTitulo.value.trim();
+    const descricao = inputDescricao.value.trim();
+    const data = calendario.value;
+
+    if (titulo === ""){
+        alert("Por favor, insera um título para a tarefa.");
+        return;
+    }
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+        <h3>${titulo}</h3>
+        <p>${descricao}</p>
+        <p>${data}</p>
+    `;
+
+    const tarefaAFazer = document.querySelector("#afazer .tarefas");
+    tarefaAFazer.appendChild(card);
+
+    inputTitulo.value = "";
+    inputDescricao.value = "";
+
+    formTarefa.classList.add("oculto");
+    colunas.style.display = "flex";
+})
 
